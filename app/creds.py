ebf09242748be918d45db27c026f2e692ef3874b
chapeter <chapeter@cisco.com>
2016-07-06 10:15:22 -0500

    clean up some code

---
+++ app/creds.py
@@ -0,0 +1,3 @@
+apicurl = 'http://10.94.238.68'
+apicuser = 'admin'
+apicpass = 'cisco123'
--- app/tasks.py
+++ app/tasks.py
@@ -3,38 +3,37 @@ __email__ = 'chapeter@cisco.com'
 
 import acitoolkit as ACI
 import sys
+import creds
 
 
-apicurl = 'http://10.94.238.68'
-apicuser = 'admin'
-apicpass = 'cisco123'
+def getHealthColor(item):
+    if item > 90:
+        item_color = 'green'
+    elif item < 80:
+        item_color = 'red'
+    else:
+        item_color = 'yellow'
+    return item_color
 
 def getHealth():
-    session = ACI.Session(apicurl, apicuser, apicpass)
+    session = ACI.Session(creds.apicurl, creds.apicuser, creds.apicpass)
     resp = session.login()
     if not resp.ok:
         print('%% Could not login to APIC')
         sys.exit(0)
 
+
+
     tenant = "ACME Prod Network"
     tenant_health = ACI.HealthScore.get_by_dn(session, 'uni/tn-acme-prod')
-    #print tenant, tenant_health
-    if tenant_health > 90:
-        tenant_bgcolor = 'green'
-    elif tenant_health < 80:
-        tenant_bgcolor = 'red'
-    else:
-        tenant_bgcolor = 'yellow'
+    tenant_bgcolor = getHealthColor(tenant)
+
+
 
     application = "People Soft Financial"
     application_health = ACI.HealthScore.get_by_dn(session, 'uni/tn-acme-prod/ap-peopleSoft-fin')
-    #print application, application_health
-    if application_health > 90:
-        app_bgcolor = 'green'
-    elif application_health < 80:
-        app_bgcolor = 'red'
-    else:
-        app_bgcolor = 'yellow'
+    app_bgcolor = getHealthColor(application_health)
+
 
     epg1 = "Web Front End"
     epg2 = "Middleware"
@@ -44,30 +43,10 @@ def getHealth():
     epg2_health = ACI.HealthScore.get_by_dn(session, 'uni/tn-acme-prod/ap-peopleSoft-fin/epg-app')
     epg3_health = ACI.HealthScore.get_by_dn(session, 'uni/tn-acme-prod/ap-peopleSoft-fin/epg-db')
 
-    if epg1_health > 90:
-        epg1_bgcolor = 'green'
-    elif epg1_health < 80:
-        epg1_bgcolor = 'red'
-    else:
-        epg1_bgcolor = 'yellow'
+    epg1_bgcolor = getHealthColor(epg1_health)
+    epg2_bgcolor = getHealthColor(epg2_health)
+    epg3_bgcolor = getHealthColor(epg3_health)
 
-    if epg2_health > 90:
-        epg2_bgcolor = 'green'
-    elif epg2_health < 80:
-        epg2_bgcolor = 'red'
-    else:
-        epg2_bgcolor = 'yellow'
-
-    if epg3_health > 90:
-        epg3_bgcolor = 'green'
-    elif epg3_health < 80:
-        epg3_bgcolor = 'red'
-    else:
-        epg3_bgcolor = 'yellow'
-
-    #print epg1, epg1_health
-    #print epg2, epg2_health
-    #print epg3, epg3_health
 
     data = [{'name' : tenant, 'health' : tenant_health, 'bgcolor' : tenant_bgcolor},
             {'name' : application, 'health' : application_health, 'bgcolor' : app_bgcolor},
